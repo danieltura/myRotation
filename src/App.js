b26f538bd975901bff7802ba09d0b14b30e4e389
danieltura <dderese1@gmail.com>
2023-06-21 15:44:20 -0400

    remove server and move client to main folder

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <p>
+          Edit It At <b>src/App.js</b> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
