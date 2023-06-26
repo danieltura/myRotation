94d896cf4ab10c32dec1bf46486a46dc5d9df7a2
dan <dan@localhost.localdomain>
2023-06-26 16:02:34 -0400

    split classes

--- src/App.js
+++ src/App.js
@@ -1,54 +1,8 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.primary,
-}));
+import Rotation from "./Rotation/Rotation";
 
 function App() {
-  return (
-    <Box
-      sx={{
-        flexGrow: 1,
-        backgroundColor: "purple",
-        height: "100vh",
-        margin: "auto",
-        padding: "2%",
-      }}
-    >
-      <Grid container spacing={5}>
-        <Grid item xs={12}>
-          <Grid container spacing={3} justifyContent="space-between">
-            <Grid item xs={2}>
-              <Item>Prev</Item>
-            </Grid>
-            <Grid item xs={2}>
-              <Item>Prev</Item>
-            </Grid>
-            <Grid item xs={2}>
-              <Item>Next</Item>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid item xs={12}>
-          <Grid container spacing={1} columns={14}>
-            {Array.from(Array(30)).map((_, index) => (
-              <Grid item xs={2} key={index}>
-                <Item>{index + 1}</Item>
-              </Grid>
-            ))}
-          </Grid>
-        </Grid>
-      </Grid>
-    </Box>
-  );
+  return <Rotation />;
 }
 
 export default App;
