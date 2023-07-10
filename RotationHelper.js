e65ace4d74954c137551b46679f50677f9c3efce
danieltura <dderese1@gmail.com>
2023-07-10 09:07:55 -0400

    Add date generator

--- RotationHelper.js
+++ RotationHelper.js
@@ -1,3 +1,4 @@
+const { DateTime } = require("luxon");
 const Rotation = require("./Rotation");
 const rotationFile = require("./test_rotation.json");
 
@@ -7,10 +8,25 @@ class RotationHelper extends Rotation {
   }
 
   getRotationByMonth() {}
+
+  generateRotation(num_of_days = 30) {
+    const keys = Object.keys(this.rotation);
+    let start_date = DateTime.fromISO("2023-07-03");
+    let format = "MMMM dd, yyyy";
+
+    for (const key of keys) {
+      for (const shift of this.rotation[key]) {
+        let type = shift.shift_type;
+        for (let index = 0; index < shift.days; index++) {
+          console.log(type + "--> " + start_date.toFormat(format));
+          start_date = start_date.plus({ days: 1 });
+        }
+      }
+    }
+  }
 }
 
 const r = new RotationHelper(rotationFile);
-console.log(r.get_first_name());
-console.log(r.get_last_name());
-console.log(r.get_rotation_start_date());
+r.generateRotation();
+
 module.exports = RotationHelper;
